13320d96a0dd0d32f4bd4c2755ed830490097bca
Callum Hughes <chughes65@googlemail.com>
2023-10-22 18:57:25 +0100

    File upload functionality

---
+++ main.py
@@ -0,0 +1,119 @@
+import flet as ft
+from flet import (
+    ElevatedButton,
+    FilePicker,
+    FilePickerResultEvent,
+    FilePickerUploadFile,
+    Page,
+    Row,
+    Text,
+    icons,
+)
+
+def main(page):
+
+    first_name = ft.TextField(label="First name", autofocus=True)
+    last_name = ft.TextField(label="Last name")
+    greetings = ft.Column()
+
+    def btn_click(e):
+        greetings.controls.append(ft.Text(f"Hello, {first_name.value} {last_name.value}!"))
+        first_name.value = ""
+        last_name.value = ""
+        page.update()
+        first_name.focus()
+
+    page.add(
+        first_name,
+        last_name,
+        ft.ElevatedButton("Say hello!", on_click=btn_click),
+        greetings,
+    )
+    
+    def pick_files_result(e: FilePickerResultEvent):
+        selected_files.value = (
+            ", ".join(map(lambda f: f.name, e.files)) if e.files else "Cancelled!"
+        )
+        selected_files.update()
+
+    pick_files_dialog = FilePicker(on_result=pick_files_result)
+    selected_files = Text()
+
+    # Save file dialog
+    def save_file_result(e: FilePickerResultEvent):
+        save_file_path.value = e.path if e.path else "Cancelled!"
+        save_file_path.update()
+
+    save_file_dialog = FilePicker(on_result=save_file_result)
+    save_file_path = Text()
+
+    # Open directory dialog
+    def get_directory_result(e: FilePickerResultEvent):
+        directory_path.value = e.path if e.path else "Cancelled!"
+        directory_path.update()
+
+    get_directory_dialog = FilePicker(on_result=get_directory_result)
+    directory_path = Text()
+    
+    def upload_files(e):
+        upload_list = []
+        if pick_files_dialog.result != None and pick_files_dialog.result.files != None:
+            for f in pick_files_dialog.result.files:
+                upload_list.append(
+                    FilePickerUploadFile(
+                        f.name,
+                        upload_url=page.get_upload_url(f.name, 600),
+                    )
+                )
+            pick_files_dialog.upload(upload_list)
+
+
+    # hide all dialogs in overlay
+    page.overlay.extend([pick_files_dialog, save_file_dialog, get_directory_dialog])
+
+    page.add(
+        Row(
+            [
+                ElevatedButton(
+                    "Pick files",
+                    icon=icons.UPLOAD_FILE,
+                    on_click=lambda _: pick_files_dialog.pick_files(
+                        allow_multiple=True
+                    ),
+                ),
+                selected_files,
+            ]
+        ),
+        Row(
+            [
+                ElevatedButton(
+                    "Save file",
+                    icon=icons.SAVE,
+                    on_click=lambda _: save_file_dialog.save_file(),
+                    disabled=page.web,
+                ),
+                save_file_path,
+            ]
+        ),
+        Row(
+            [
+                ElevatedButton(
+                    "Open directory",
+                    icon=icons.FOLDER_OPEN,
+                    on_click=lambda _: get_directory_dialog.get_directory_path(),
+                    disabled=page.web,
+                ),
+                directory_path,
+            ]
+        ),
+        Row(
+            [
+                ft.ElevatedButton(
+                    "Upload",
+                    on_click=upload_files
+                ),
+            ]
+        )
+    )
+
+ft.app(target=main)
